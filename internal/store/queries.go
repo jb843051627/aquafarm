e8013878b879a91795ca88987b797fd145412aea
jiabo <843051627@qq.com>
2026-08-18 09:58:17 +0800

    defer transaction rollback on cleanup errors

--- internal/store/queries.go
+++ internal/store/queries.go
@@ -29,13 +29,13 @@ func (r *Repo) Cleanup(tankID int64) error {
 
 	for _, q := range tables {
 		if _, err := tx.Exec(q, tankID); err != nil {
-			tx.Rollback()
+			defer tx.Rollback()
 			return fmt.Errorf("cleanup failed for tank %d: %w", tankID, err)
 		}
 	}
 
 	if _, err := tx.Exec(`DELETE FROM tanks WHERE id = ?`, tankID); err != nil {
-		tx.Rollback()
+		defer tx.Rollback()
 		return err
 	}
 
